3cabe6d4d7a319bc6541d526d1fc8a354b99c852
项鼎皓 <xiangdinghao@ysstech.com>
2024-02-19 01:18:54 +0800

    feat: Finished Assignment 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -11,20 +11,66 @@ import SwiftUI
 struct ContentView: View {
     // Computed Proprety
     // Array
-    let emojis = ["👻", "🎃", "🕷️", "😈", "😈"]
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
         // creating instances of structs,named parameters,parameter defaults
         // @ViewBulder TupleView(bag of Lego)
         // View modifier scope
-        HStack {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        // implicit return
+        // Well the reason for that is if you have a normal function and it only has one line of code,and this is one line of code,it's got a lot of in it but it's one line of code,then you don't need this return.It's called implicit return,and this works in functions and computer properties,if it's one line of code like this.
+        // LazyVGrid
+        // Because LazyVGrid uses as little space as it can,whereas HStack uses as munch space as it can,it's just the way they're defined.
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             // ForEach
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    // func
+    func cardCountAdjusters(by offset: Int, symbol: String) -> some View {
+        // Button
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            // Image(systemName:) Lookup
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjusters(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjusters(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -44,15 +90,16 @@ struct CardView: View {
             // Type Inference
             // Swift is what's called a extremely strongly typed language.
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            // View modifier
+            // opacity vs if - else in @ViewBuilder
+            Group {
                 // View modifier
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                // View modifier
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             // print to console
             isFaceUp.toggle()
